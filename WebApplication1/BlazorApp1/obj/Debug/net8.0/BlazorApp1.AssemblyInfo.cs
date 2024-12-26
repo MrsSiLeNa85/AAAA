2f109353f00409e4e85ea4b0b734baf6e31e7082
Mrs_SiLeNa_85 <cozlova.nadiusha@yandex.ru>
2024-12-26 14:59:49 +0300

    божеспаси
    ыыых

--- WebApplication1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ WebApplication1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338f4998b0a4944faa90ca86ed0247d651e75753")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
